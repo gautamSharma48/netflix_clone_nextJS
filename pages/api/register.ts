508fa3cfbfe9dea2f9e8fdd52d0117d1080c1095
lhtgautamsharma <95333485+Gautam-Sharma-leewayhertz@users.noreply.github.com>
2024-03-15 19:42:13 +0530

    changes

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -6,9 +6,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // if (req.method !== "POST") {
-  //   return res.status(405).end();
-  // }
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
   try {
     const { email, name, password } = req.body;
     const existingUser = await prismadb.user.findUnique({
